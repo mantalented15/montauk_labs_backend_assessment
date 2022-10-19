fac48cb03219f35cac0a91f0fed2ebd12287e92e
unknown <mantalented15@gmail.com>
2022-10-19 10:37:14 +0800

    WebDriverWait resolved...

--- selenium_test.py
+++ selenium_test.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
 
 driver = webdriver.Chrome('chromedriver')
 driver.get('https://transparency-in-coverage.uhc.com/')
 
-time.sleep(80)
+# time.sleep(80)
 # a = driver.find_elements_by_xpath('//li[@data-testid="list-item-8"]//div[@class="ant-space-item"]')
 # a = driver.find_elements_by_xpath('//li[@data-testid="list-item-8"]')
-a = driver.find_element(By.XPATH, '//a[@href="https://uhc-tic-mrf.azureedge.net/public-mrf/2022-10-01/2022-10-01_-Big-Valley-Construction-LLC_index.json"]')
+# a = driver.find_element(By.XPATH, '//div[@class="ant-space-item"]/a')
+# a = driver.find_element(By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')
+
+WebDriverWait(driver,240).until(EC.element_to_be_clickable((By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')))
+a = driver.find_element(By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')
+
 # a_list = []
 # for i in range(len(a)):
 #     a_list.append(a[i].text)
