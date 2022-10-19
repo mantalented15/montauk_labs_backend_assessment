d2fcfb148c15a04882a65fbf1aa81d684d05d541
unknown <mantalented15@gmail.com>
2022-10-19 17:20:16 +0800

    href usage finished...

--- selenium_test.py
+++ selenium_test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import time
+import requests
 
 driver = webdriver.Chrome('chromedriver')
 driver.get('https://transparency-in-coverage.uhc.com/')
@@ -15,12 +16,23 @@ driver.get('https://transparency-in-coverage.uhc.com/')
 # a = driver.find_element(By.XPATH, '//div[@class="ant-space-item"]/a')
 # a = driver.find_element(By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')
 
-WebDriverWait(driver,240).until(EC.element_to_be_clickable((By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')))
-a = driver.find_element(By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')
+# WebDriverWait(driver,240).until(EC.element_to_be_clickable((By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')))
+# a = driver.find_element(By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')
+try:
+    # a = WebDriverWait(driver,180).until(EC.presence_of_element_located((By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')))
+    a = WebDriverWait(driver,180).until(EC.visibility_of_element_located((By.XPATH, '//li[@data-testid="list-item-1"]/div/div/a')))
+    href = a.get_attribute('href')
+    # a = WebDriverWait(driver,180).until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="ant-space-item"]/a')))
+    # a = WebDriverWait(driver,180).until(EC.presence_of_all_elements_located((By.XPATH, '//ul[@class="ant-list-items"]')))
+except:
+    print("Timed out waiting for page to load")
+
 
 # a_list = []
 # for i in range(len(a)):
 #     a_list.append(a[i].text)
 
-print(a.text)
+# print(a.text)
+# print(a.get_attribute("href"))
 # print(a_list)
+print(requests.get(href).text)
